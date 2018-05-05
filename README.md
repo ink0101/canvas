3198cc130e07e2042ee1cfd156fce4e1e714e419
ink0101 <cftt2016@gmail.com>
2018-05-05 21:49:33 +0800

    [add] 绘制线条

---
+++ README.md
@@ -0,0 +1,18 @@
+## canvas 学习笔记
+
+### 绘制画布
+`<canvas id="#canvas"> </canvas>`
+### 获取画布
+> 根据id获取
+
+### 获取画笔
+`const context = canvas.getContext('2d');`
+#### canvas 画笔 常用方法
+
+`
+context.moveTo(x, y)
+context.lineTo(x, y)
+context.lineWidth
+context.strokeStyle
+`
+
--- js/demo01.js
+++ js/demo01.js
@@ -1,4 +1,18 @@
 const canvas = document.getElementById('canvas');
+canvas.width = 800;
+canvas.height = 600;
 // 获取画笔
 const context = canvas.getContext('2d');
 console.log(context);
+
+// debugger
+// 移动画笔
+context.moveTo(100, 100);
+// 笔画停点
+context.lineTo(300, 300);
+// 笔画宽度
+context.lineWidth = 5;
+// 描边颜色
+context.strokeStyle = "#aa394c";
+
+context.stroke();
